5a636d58a0aef33cf1c6f07d6e8a07efc22b4c54
manzanofp <felipemanzanofp@gmail.com>
2022-01-10 12:14:44 -0300

    Project created

---
+++ backend/src/main/java/com/manzanofp/bsgames/BsgamesApplication.java
@@ -0,0 +1,13 @@
+package com.manzanofp.bsgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsgamesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/manzanofp/bsgames/BsgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manzanofp.bsgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
